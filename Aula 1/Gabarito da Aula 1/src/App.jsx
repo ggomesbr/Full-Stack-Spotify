1b1b14395fa259fbaea49ab2d0782b9a324a9c7c
Gabriel Luiz de Souza Gomes <gabriel.gomes@novacap.df.gov.br>
2025-02-12 12:13:23 -0300

    Inicio da implementação da main

--- Aula 1/Gabarito da Aula 1/src/App.jsx
+++ Aula 1/Gabarito da Aula 1/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import Header from "./components/Header";
+import Main from "./components/main";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App
